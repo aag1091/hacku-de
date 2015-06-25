8df6b48ce14691822d19e25362ab7d694a88d15b
Heath Padrick <heath.padrick@dominionmarinemedia.com>
2015-06-25 15:59:34 -0400

    updated stubbed payload for metrics

--- routes/metrics.js
+++ routes/metrics.js
@@ -1,13 +1,28 @@
+var _ = require('lodash');
+var step = require('step');
+var metrics = require('../models/metrics');
 
 module.exports = function(app) {
 
   app.get('/metrics', function(req, res) {
-    res.send([
-        makeMetric("app-score"),
-        makeMetric("page-speed"),
-        makeMetric("unique-visits"),
-        makeMetric("app-errors")
-    ]);
+    var metrics = makeMetric("app-score", ",")
+                + makeMetric("page-speed", ",")
+                + makeMetric("unique-visits", ",")
+                + makeMetric("app-errors");
+
+    res.send(metrics);
+  });
+
+  app.get('/metrics-working', function(req, res, next) {
+    step(
+        function() {
+          metrics.latestPayload(this);
+        },
+        function(err, payload) {
+          if(err) next(err);
+          res.send(payload);
+        }
+    )
   });
 
 };
@@ -17,12 +32,12 @@ function randomScore(min, max) {
   return Math.ceil(Math.random() * (max - min) + min);
 }
 
-function makeMetric(name) {
+function makeMetric(name, suffix) {
   var score = randomScore(0 ,180);
-  return {
-    name: name,
-    score: score,
-    alert: score < 50
-  };
+
+  return score + ", " + boolToString(score < 50) + (suffix ? suffix : "");
 }
 
+function boolToString(b) {
+  return b ? 1 : 0;
+}
