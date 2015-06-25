c8b28c649a1f06cd6194e14a8f2602c8aab245f8
Heath Padrick <heath.padrick@dominionmarinemedia.com>
2015-06-25 10:07:39 -0400

    added stub metrics

--- routes/common.js
+++ routes/common.js
@@ -1,10 +1,51 @@
 
 module.exports = function(app) {
 
-  app.get('/', function(req, res, next) {
+  app.get('/', function(req, res) {
     res.send({
       project: "ROSS"
     });
   });
 
+  app.get('/metrics', function(req, res) {
+    res.send([
+      {
+        name: "conversion",
+        range: {
+          min: 1,
+          max: 10
+        },
+        score: randomScore(1, 10)
+      },
+      {
+        name: "page_speed",
+        range: {
+          min: 1,
+          max: 10
+        },
+        score: randomScore(1, 10)
+      },
+      {
+        name: "new_relic",
+        range: {
+          min: 1,
+          max: 10
+        },
+        score: randomScore(1, 10)
+      },
+      {
+        name: "app_rating",
+        range: {
+          min: 1,
+          max: 10
+        },
+        score: randomScore(1, 10)
+      }
+    ]);
+  });
+
 };
+
+function randomScore(min, max) {
+  return Math.ceil(Math.random() * (max - min) + min);
+}
